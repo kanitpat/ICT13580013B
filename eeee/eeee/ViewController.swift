6689c20bb28512fd4561ad9fd92c1dbe811c790c
smart <32380846+ApiwanN@users.noreply.github.com>
2017-12-06 16:21:57 +0700

    งานชิ้นแรก

---
+++ eeee/eeee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eeee
+//
+//  Created by ict on 9/24/2560 BE.
+//  Copyright © 2560 ict. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
